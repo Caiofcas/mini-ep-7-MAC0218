78b3f9c358311650dd8f3f4d92823215afaadbe2
ccastro <caiofcastro@usp.br>
2022-05-11 17:07:08 -0300

    add behaviour

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,4 +1,7 @@
 package domain
+import domain.criterios.CriterioDeAprovacao
+import domain.Boletim
+import domain.BoletimFechado
 
 class AnalisadorDeAprovacao {
 
@@ -11,5 +14,38 @@ class AnalisadorDeAprovacao {
     //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
     //
     // ---------------------------------
+
+    lateinit var criterio_atual: CriterioDeAprovacao
+
+    fun defineCriterio(criterio: CriterioDeAprovacao) {
+        criterio_atual = criterio
+    }
+
+    fun fechaBoletim(boletim: Boletim): BoletimFechado {
+        return BoletimFechado(
+            mediaFinal=criterio_atual.mediaFinal(boletim), 
+            foiAprovado=criterio_atual.estaAprovado(boletim), 
+            mediaEPs=boletim.mediaEPs, 
+            mediaMiniEPs=boletim.mediaMiniEPs
+        )
+    }
+
+    fun mediaMiniEPs(): Double {
+        return 0.0
+    }
+
+    fun mediaEPs(): Double {
+        return 0.0
+
+    }
+
+    fun mediaFinal(): Double {
+        return 0.0
+
+    }
+
+    fun foiAprovado(): Boolean {
+        return false
+    }
 
 }
